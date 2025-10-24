b4fcdea0e48f9b03812013e17ccc177b75429574
MEKAKO HAMU GENAMO <makakohm6@gmail.com>
2025-10-24 09:41:48 +0300

    Optimize smallerNumbersThanCurrent method

--- How Many Numbers Are Smaller Than the Current Number.py
+++ How Many Numbers Are Smaller Than the Current Number.py
@@ -1,11 +1,7 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)
         Output = []
         for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[j] < nums[i]:
-                    count += 1
-            Output.append(count)
+            Output.append(sorted_nums.index(nums[i]))
         return Output
-        
